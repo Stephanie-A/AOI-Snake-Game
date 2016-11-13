0ce7b194a87f034e41cc62ee3a2572d8ddc6b91f
Stephanie A <almeidasmail@gmail.com>
2016-11-13 18:14:02 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Drawing;
+using System.Windows.Threading;
+using System.Windows.Navigation;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Only issue is multiple warning boxes pop up
+namespace AOIGameSnake
+{
+    
+    
+
+    public partial class Window1 : Window
+    {
+       // public int numLives = Convert.ToInt32(Application.Current.Properties["NumLeft"]);
+        //public static int ToInt32(object value);
+        public int score = Convert.ToInt32(Application.Current.Properties["Score"]);
+        public Window1()
+        {
+            InitializeComponent();
+            
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+
+            /* Here user can change the speed of the snake. 
+             * Possible speeds are FAST, MODERATE, SLOW and DAMNSLOW */
+            timer.Interval = MODERATE;
+            timer.Start();
+
+            //Application.Current.Properties["Score"] = 0;
+
+            this.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+            paintSnake(startingPoint);
+            currentPosition = startingPoint;
+
+            // Instantiate Food Objects
+            for (int n = 0; n < 10; n++)
+            {
+                paintBonus(n);
+            }
+        }
+
+        // This list describes the Bonus Red pieces of Food on the Canvas
+        private List<Point> bonusPoints = new List<Point>();
+
+        // This list describes the body of the snake on the Canvas
+        private List<Point> snakePoints = new List<Point>();
+
+
+        
+
+
+        private Brush snakeColor = Brushes.Green;
+        private enum SIZE
+        {
+            THIN = 4,
+            NORMAL = 6,
+            THICK = 8
+        };
+        private enum MOVINGDIRECTION
+        {
+            UPWARDS = 8,
+            DOWNWARDS = 2,
+            TOLEFT = 4,
+            TORIGHT = 6
+        };
+
+        private TimeSpan FAST = new TimeSpan(1);
+        private TimeSpan MODERATE = new TimeSpan(10000);
+        private TimeSpan SLOW = new TimeSpan(50000);
+        private TimeSpan DAMNSLOW = new TimeSpan(500000);
+
+
+
+        private Point startingPoint = new Point(100, 100);
+        private Point currentPosition = new Point();
+
+        // Movement direction initialisation
+        private int direction = 0;
+
+        /* Placeholder for the previous movement direction
+         * The snake needs this to avoid its own body.  */
+        private int previousDirection = 0;
+
+        /* Here user can change the size of the snake. 
+         * Possible sizes are THIN, NORMAL and THICK */
+        private int headSize = (int)SIZE.THICK;
+
+
+
+        private int length = 100;
+        //private int score = Application.Current.Properties["Score"];
+        private Random rnd = new Random();
+
+        private void paintSnake(Point currentposition)
+        {
+
+            /* This method is used to paint a frame of the snake´s body
+             * each time it is called. */
+
+                Ellipse newEllipse = new Ellipse();
+                newEllipse.Fill = snakeColor;
+                newEllipse.Width = headSize;
+                newEllipse.Height = headSize;
+
+                Canvas.SetTop(newEllipse, currentposition.Y);
+                Canvas.SetLeft(newEllipse, currentposition.X);
+
+                int count = paintCanvas.Children.Count;
+                paintCanvas.Children.Add(newEllipse);
+                snakePoints.Add(currentposition);
+
+
+                // Restrict the tail of the snake
+                if (count > length)
+                {
+                    paintCanvas.Children.RemoveAt(count - length + 9);
+                    snakePoints.RemoveAt(count - length);
+                }
+            
+        }
+
+
+        private void paintBonus(int index)
+        {
+            
+                Point bonusPoint = new Point(rnd.Next(5, 620), rnd.Next(5, 380));
+
+
+
+                Ellipse newEllipse = new Ellipse();
+                newEllipse.Fill = Brushes.Red;
+                newEllipse.Width = headSize;
+                newEllipse.Height = headSize;
+
+                Canvas.SetTop(newEllipse, bonusPoint.Y);
+                Canvas.SetLeft(newEllipse, bonusPoint.X);
+                paintCanvas.Children.Insert(index, newEllipse);
+                bonusPoints.Insert(index, bonusPoint);
+            
+
+        }
+
+        public static bool IsWindowOpen<Window2>(string name = "") where Window2 : Window
+        {
+            return string.IsNullOrEmpty(name) ? Application.Current.Windows.OfType<Window2>().Any()
+                : Application.Current.Windows.OfType<Window2>().Any(w => w.Name.Equals(name));
+        } 
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Expand the body of the snake to the direction of movement
+
+
+                switch (direction)
+                {
+                    case (int)MOVINGDIRECTION.DOWNWARDS:
+                        currentPosition.Y += 1;
+                        paintSnake(currentPosition);
+                        break;
+                    case (int)MOVINGDIRECTION.UPWARDS:
+                        currentPosition.Y -= 1;
+                        paintSnake(currentPosition);
+                        break;
+                    case (int)MOVINGDIRECTION.TOLEFT:
+                        currentPosition.X -= 1;
+                        paintSnake(currentPosition);
+                        break;
+                    case (int)MOVINGDIRECTION.TORIGHT:
+                        currentPosition.X += 1;
+                        paintSnake(currentPosition);
+                        break;
+                }
+            
+
+            // Restrict to boundaries of the Canvas
+            if ((currentPosition.X < 5) || (currentPosition.X > 620) ||
+                (currentPosition.Y < 5) || (currentPosition.Y > 380))
+            {
+                GameOver();
+                this.Close();
+                //Application.Current.Windows[1].Close();
+                //Application.Current.Windows[2].Close();
+               // this.Close();
+            }
+
+            
+
+            // Hitting a bonus Point causes the lengthen-Snake Effect
+            int n = 0; 
+            foreach (Point point in bonusPoints)
+            {
+
+                if ((Math.Abs(point.X - currentPosition.X) < headSize) &&
+                    (Math.Abs(point.Y - currentPosition.Y) < headSize))
+                {
+                    length += 10;
+                    score += 10;
+
+                    // In the case of food consumption, erase the food object
+                    // from the list of bonuses as well as from the canvas
+
+                   /* Window2 win2 = new Window2();
+                    win2.Show();*/
+                    //this.Hide();
+
+
+
+                        bonusPoints.RemoveAt(n);
+                        paintCanvas.Children.RemoveAt(n);
+                        paintBonus(n);
+                        break;
+                    
+                }
+                n++;
+            }
+
+            // Restrict hits to body of Snake
+
+
+            for (int q = 0; q < (snakePoints.Count - headSize * 2); q++)
+            {
+
+                    Point point = new Point(snakePoints[q].X, snakePoints[q].Y);
+                    if ((Math.Abs(point.X - currentPosition.X) < (headSize)) &&
+                         (Math.Abs(point.Y - currentPosition.Y) < (headSize)))
+                    {
+                        GameOver();
+                       // break;
+                    }
+                
+
+            }
+
+
+
+
+        }
+
+
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            
+                switch (e.Key)
+                {
+
+                    case Key.Down:
+                        if (previousDirection != (int)MOVINGDIRECTION.UPWARDS)
+                            direction = (int)MOVINGDIRECTION.DOWNWARDS;
+                        break;
+                    case Key.Up:
+                        if (previousDirection != (int)MOVINGDIRECTION.DOWNWARDS)
+                            direction = (int)MOVINGDIRECTION.UPWARDS;
+                        break;
+                    case Key.Left:
+                        if (previousDirection != (int)MOVINGDIRECTION.TORIGHT)
+                            direction = (int)MOVINGDIRECTION.TOLEFT;
+                        break;
+                    case Key.Right:
+                        if (previousDirection != (int)MOVINGDIRECTION.TOLEFT)
+                            direction = (int)MOVINGDIRECTION.TORIGHT;
+                        break;
+
+                }
+                previousDirection = direction;
+            
+
+        }
+
+
+
+        private void GameOver()
+        {
+            //numLives = 5 - Convert.ToInt32(Application.Current.Properties["NumLeft"]);
+            //numLives -= 1;
+            
+                MessageBoxResult result = MessageBox.Show("Save Yourself?", "Try Again?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                Application.Current.Properties["Score"] = score;
+                //Application.Current.Properties["NumLeft"] = numLives;
+
+                if (result== MessageBoxResult.Yes) 
+                {
+                    
+                        Window2 win2 = new Window2();
+                        win2.Show();
+                        this.Close();
+                        System.Windows.Threading.Dispatcher.Run();
+                    
+                        
+                }
+
+                else //if (result == MessageBoxResult.No)
+                {
+                    MessageBox.Show("Your score was " + score, "Game Over!", MessageBoxButton.OK);
+                    //Application.Current.Shutdown();
+                    this.Close();
+                }
+
+         
+                //break;
+
+            
+
+            
+        }
+
+        /*class Question
+        {
+            private string question;
+            //private string aoiType;
+            //private int questionNum;
+            private string image;
+            // private int answer;
+
+            //public Question(string q = "", /*string aoi, int num/*, int a,*/ /*string i = "")*/
+            /*{
+                this.question = q;
+                //this.aoiType = aoi;
+                //this.questionNum = num;
+                this.image = i;
+                //this.answer = a;
+            }
+
+            public Question()
+            {
+                question = "";
+                image = "";
+            }
+
+            public Question(string i)
+            {
+                this.image = i;
+            }
+
+            public string returnQuestion()
+            {
+                return question;
+            }
+
+           /* public int returnQuestionNum()
+            {
+                return questionNum;
+            }
+
+
+            public string returnImage()
+            {
+                return image;
+            }
+        }*/
+    }
+}
+
---
+++ Window2.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using System.Object;
+namespace AOIGameSnake
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        private static string imageSet;
+        private static string answerSet;
+        //private static string answerCk;
+        public Window2()
+        {
+            InitializeComponent();
+            //Question question1 = new Question("If A and B are both a logical '1', what is the output?", 1);
+
+            //textBox.Text = question1.returnQuestion();
+
+            //button1.Content = "'1'";
+            //button2.Content = "'0'";
+
+            //Application.Current.Properties["Score"] = 0;
+
+
+            //scoreBox.Text = "Score: " + Application.Current.Properties["Score"];
+
+            //int randObject = 
+
+            
+            //to generate random question
+            Random rand = new Random();
+            int randObject = rand.Next(5);
+
+            if (randObject == 0)
+            {
+                Question question1 = new Question("If A and B are both a logical '1', what is the output?", "C:\\Users\\S\\Pictures\\symbol-of-and-gate.png", "'1'");
+                //question1.Image_Loaded();
+                imageSet = question1.returnImage();
+                textBox.Text = question1.returnQuestion();
+                answerSet = question1.returnAnswer();
+                //textBox.Text = answerSet;
+            }
+
+            else if (randObject == 1)
+            {
+                Question question2 = new Question("If A is a logical '1' and B is a logical '0', what is the output?", "C:\\Users\\S\\Pictures\\2000px-Or-gate-en.svg.png", "'1'");
+                //question1.Image_Loaded();
+                imageSet = question2.returnImage();
+                textBox.Text = question2.returnQuestion();
+                answerSet = question2.returnAnswer();
+            }
+
+            else if (randObject == 2)
+            {
+                Question question3 = new Question("If A1 and B1 are both '0', what is Y1?", "C:\\Users\\S\\Pictures\\NORGate.png", "'1'");
+                //question1.Image_Loaded();
+                imageSet = question3.returnImage();
+                textBox.Text = question3.returnQuestion();
+                answerSet = question3.returnAnswer();
+            }
+
+            else if (randObject == 3)
+            {
+                Question question4 = new Question("If A is a 1 and B and C are a '0', What is the output?", "C:\\Users\\S\\Pictures\\Circuit4.png", "'0'");
+                //question1.Image_Loaded();
+                imageSet = question4.returnImage();
+                textBox.Text = question4.returnQuestion();
+                answerSet = question4.returnAnswer();
+            }
+
+            else if (randObject ==4)
+            {
+                Question question5 = new Question("Which logic gate does this truth table represent?", "C:\\Users\\S\\Pictures\\2-Input-NAND-Gate-Truth-Table.jpg", "NAND");
+                //question1.Image_Loaded();
+                imageSet = question5.returnImage();
+                textBox.Text = question5.returnQuestion();
+                answerSet = question5.returnAnswer();
+            }
+        }
+
+        
+        /*private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Correct Answer!", "Correct!", MessageBoxButton.OK);
+
+            Application.Current.Properties["Score"] = 1;
+            scoreBox.Text = "Score: " + Application.Current.Properties["Score"];
+
+            //Question question2 = new Question("If A is a logical '1' and B is a logical '0', what is the output?", 2, "C:\\Users\\S\\Pictures\\2000px-Or-gate-en.svg.png");
+
+            //textBox.Text = question2.returnQuestion();
+
+           /* BitmapImage c = new BitmapImage();
+            c.BeginInit();
+            c.UriSource = new Uri(question2.returnImage());
+            c.EndInit();
+            var image = sender as Image;
+            image.Source = c;*/
+
+           // SetImage("C:\\Users\\S\\Pictures\\2000px-Or-gate-en.svg.png");
+
+            /*Window win2 = new Window();
+            win2.Show();
+            this.Close();*/
+
+            //string score = Application.Current.Properties["Score"];
+
+            /*Window2 win2 = new Window2();
+            win2.Show();
+            this.Hide();
+
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Incorrect Answer!", "Incorrect!", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+
+            //button2.Visibility = 0;
+            
+        }*/
+
+        public void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage b = new BitmapImage();
+            //Question question1 = new Question("C:\\Users\\S\\Pictures\\symbol-of-and-gate.png");
+            b.BeginInit();
+            b.UriSource = new Uri(imageSet);
+            b.EndInit();
+            var image = sender as Image;
+            image.Source = b;
+
+            /*Image img = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            Uri uri = new Uri(imageSet);
+            bitmapImage.UriSource = uri;
+            img.Source = bitmapImage;*/
+        }
+
+        private void SetImage(string path)
+        {
+            image.BeginInit();
+            image.Source = new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+            image.EndInit();
+        }
+
+        private void Submitbtn_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Windows.Application.Current.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,(System.Threading.ThreadStart)delegate { answerCk = AnswerBox.Text; });
+            //textBox.Text = answerCk;
+            string answerCk = UserInput.ToString();
+            //textBox.Text = answerCk;
+            if (answerCk == "System.Windows.Controls.TextBox: " + answerSet)
+            {
+                MessageBox.Show("Correct Answer!", "Correct!", MessageBoxButton.OK);
+                Window1 win1 = new Window1();
+                win1.Show();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Wrong Answer, Try Again!", "Incorrect!", MessageBoxButton.OK);
+            }
+            //textBox.Text = AnswerBox.Text;
+        }
+    }
+
+
+
+
+    class Question
+    {
+        private string question;
+        //private string aoiType;
+        //private int questionNum;
+        private string image;
+        //private int lives;
+        private string answer;
+
+        public Question(string q, string i, string a)
+        {
+            this.question = q;
+            //this.aoiType = aoi;
+            //this.questionNum = num;
+            this.image = i;
+            this.answer = a;
+            //lives = l;
+        }
+
+        public Question()
+        {
+            this.question = "";
+            this.image = "";
+            this.answer = "";
+            //lives = 0;
+        }
+
+        public Question(string i)
+        {
+            this.image = i;
+        }
+
+        public string returnQuestion()
+        {
+            return question;
+        }
+
+        /*public int returnQuestionNum()
+        {
+            return questionNum;
+        }*/
+
+
+        public string returnImage()
+        {
+            return image;
+        }
+
+        public string returnAnswer()
+        {
+            return answer;
+        }
+
+    }
+}
+
